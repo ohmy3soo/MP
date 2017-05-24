cd22470cdc6d54238a2b1b0ef860697abdb572cc
ohmy3soo <ohmy3soo@nate.com>
2017-05-25 02:05:57 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,73 +1,88 @@
-package org.towm.android.a170418lab;
+package org.towm.android.lab6;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    RadioGroup radioGroup;
-    RadioButton male;
-    RadioButton female;
-    Intent intent;
-    CheckBox sms;
-    CheckBox eMail;
-
+    EditText txtData;
+    Button writeButton;
+    Button clearButton;
+    Button readButton;
+    Button finishButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        editText = (EditText) findViewById(R.id.name);
-        radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
-        male = (RadioButton) findViewById(R.id.male);
-        female = (RadioButton) findViewById(R.id.female);
-        intent = new Intent(getApplicationContext(), RegisterActivity.class);
-        sms = (CheckBox) findViewById(R.id.sms);
-        eMail = (CheckBox) findViewById(R.id.email);
+        txtData = (EditText)findViewById(R.id.txtData);
+        writeButton = (Button)findViewById(R.id.writeButton);
+        clearButton = (Button)findViewById(R.id.clearButton);
+        readButton = (Button)findViewById(R.id.readButton);
+        finishButton = (Button)findViewById(R.id.finishButton);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
+        writeButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                File sdCard = Environment.getExternalStorageDirectory();
+                File directory = new File (sdCard.getAbsolutePath() + "/MyFiles");
+                directory.mkdirs();
+                File file = new File(directory, "textfile.txt");
+                try{
+                    FileOutputStream fOut = new FileOutputStream(file);
+                    OutputStreamWriter osw = new OutputStreamWriter(fOut);
+                    osw.write(txtData.getText().toString());
+                    osw.close();
+                } catch (Throwable t) {}
+                Toast.makeText(getApplicationContext(), "Done writing SD 'textfile.txt'", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-                //Name
-                String name = editText.getText().toString();
-                intent.putExtra("name", name);
+        clearButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                txtData.setText("");
+            }
+        });
 
-                //Sex
-                int radioId = radioGroup.getCheckedRadioButtonId();
-                if (female.getId() == radioId) {
-                    intent.putExtra("sex", "여");
-                }
-                if (male.getId() == radioId) {
-                    intent.putExtra("sex", "남");
-                }
+        readButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                File sdCard = Environment.getExternalStorageDirectory();
+                File directory = new File (sdCard.getAbsolutePath() + "/MyFiles");
+                directory.mkdirs();
 
-                //Reception
-                String str = "";
-                if(sms.isChecked()){
-                    str +=  "SMS";
-                }
-                if(eMail.isChecked()){
-                    //check whether of not & is necessary
-                    if(!str.equals(""))
-                        str += " & ";
-                    str += "e-mail";
-                }
-                intent.putExtra("reception", str);
+                File file = new File(directory, "textfile.txt");
+                try {
+                    FileInputStream fIn = new FileInputStream(file);
+                    InputStreamReader isr = new InputStreamReader(fIn);
+                    BufferedReader reader = new BufferedReader(isr);
+                    String str="";
+                    StringBuffer buf = new StringBuffer();
+                    while((str = reader.readLine()) != null){
+                        buf.append(str + "\n");
+                    }
+                    isr.close();
+                    txtData.setText(buf.toString());
+                } catch (Throwable t) {}
+                Toast.makeText(getApplicationContext(), "Done reading SD 'textfile.txt'", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-                //Call new activity
-                startActivity(intent);
+        finishButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                finish();
             }
         });
     }
 }
-
